ee0957d2bd760bef02b1a4899280e640096619ae
KeplerBoyce <keplerboy@gmail.com>
2024-11-07 14:19:12 -0500

    resolve merge problem

--- client/app/ArtistBlock.tsx
+++ client/app/ArtistBlock.tsx
@@ -26,16 +26,6 @@ export default function ArtistBlock(props: {
   }, [flipped]);
 
   return (
-    <div className="min-w-72 h-90 flex flex-col rounded-xl bg-faint-gray-light dark:bg-faint-gray-dark p-4">
-      <p className="text-2xl font-bold">
-        {num}
-      </p>
-      <p className="text-lg mb-4">
-        {artist}
-      </p>
-      {/*<div className="w-full h-full bg-error rounded-lg" />*/}
-        <img src={imageUrl} className="w-full h-full object-cover rounded-lg" />
-    </div>
     <button
       className="min-w-72 h-72 group"
       onClick={() => setFlipped(!flipped)}
@@ -65,7 +55,7 @@ export default function ArtistBlock(props: {
           <p className="text-lg mb-4">
             {artist}
           </p>
-          <div className="w-full h-full bg-error rounded-lg" />
+          <img src={imageUrl} className="w-full h-full object-cover rounded-lg" />
         </>}
       </div>
     </button>
