ff0308d6f6855ac59279153dc7fe49f3e8ef62d0
Junbin Yang <junbinyang26@gmail.com>
2024-11-29 01:20:22 -0500

    finished logic of game --  stil need to test

--- server/games/views.py
+++ server/games/views.py
@@ -9,6 +9,8 @@ from rest_framework import status
 from rest_framework.response import Response
 from .util import update_or_create_user_tokens, is_spotify_authenticated, get_user_tokens
 from django.http import HttpResponseRedirect
+from members.models import Profile, Artist, Genre, UserGenre
+import random
 
 # import database from spotify/models.py
 #from spotify.models import User
@@ -38,3 +40,85 @@ class TopArtistGuessingGame(APIView):
     def post(self, request, format=None):
         # Call this when a user submits a guess
         # TODO: make a serialzer
+
+class HigherOrLowerGame(APIView):
+    def get(self, request):
+        # get user profile... might have to change to spotify ID if needed
+        user_profile = get_object_or_404(Profile, user=request.user)
+        user_artists = user_profile.artists.all()
+
+        if len(user_artists) < 2:
+            return Response({
+                "message": "Not enough artists to play the game."
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        # get a random artists followers count
+        artist1, artist2 = random.sample(user_artists, 2)
+        
+        # choose a random artist for question
+        if random.choice([True, False]):
+            question_artist = artist1
+            other_artist = artist2
+        else:
+            question_artist = artist2
+            other_artist = artist1
+
+        question_data = {
+            'question': f"Does {question_artist.name} have more followers than {other_artist.name}?",
+            'artist_name': question_artist.name,
+            'other_artist_name': other_artist.name,
+            'artist_followers': question_artist.popularity,
+            'other_artist_followers': other_artist.popularity
+        }
+        
+        # Initialize the game state if not already started
+        if not hasattr(user_profile, 'game_score'):
+            user_profile.game_score = 0
+            user_profile.save()
+
+        # Send the current game data and score
+        return Response({
+            'question': question_data['question'],
+            'game_score': user_profile.game_score
+        }, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        user_profile = get_object_or_404(Profile, user=request.user)
+        
+        # getting answers from the request data
+        answer = request.data.get('answer')  # 'higher' or 'lower'
+        artist_name = request.data.get('artist_name')
+        other_artist_name = request.data.get('other_artist_name')
+        artist_followers = request.data.get('artist_followers')
+        other_artist_followers = request.data.get('other_artist_followers')
+
+        correct_answer = 'higher' if artist_followers > other_artist_followers else 'lower'
+
+        if answer == correct_answer:
+            # Correct answer, increase score and continue game
+            user_profile.game_score += 1
+            user_profile.save()
+
+            # Generate the next question (same logic as in GET method)
+            artist1 = random.choice(Artist.objects.all())
+            artist2 = random.choice(Artist.objects.exclude(id=artist1.id))  # make sure it's not the same artist
+            question_artist, other_artist = (artist1, artist2) if random.choice([True, False]) else (artist2, artist1)
+
+            next_question_data = {
+                'question': f"Does {question_artist.name} have more followers than {other_artist.name}?",
+                'artist_name': question_artist.name,
+                'other_artist_name': other_artist.name,
+                'artist_followers': question_artist.popularity,
+                'other_artist_followers': other_artist.popularity
+            }
+            return Response({
+                'question': next_question_data['question'],
+                'game_score': user_profile.game_score
+            }, status=status.HTTP_200_OK)
+        else:
+            # reset score cus wrong answer
+            user_profile.game_score = 0
+            user_profile.save()
+            return Response({
+                'message': 'Game Over! Your score is: ' + str(user_profile.game_score)
+            }, status=status.HTTP_200_OK)
