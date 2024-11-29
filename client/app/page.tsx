911cd1cb4ca0e57d435a95d2ec65497e394b6dc9
flieger <sarangrsuman@gmail.com>
2024-11-29 12:37:35 -0500

    integrated top albums feature

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -59,9 +59,11 @@ export default function Home() {
         dataArtists = dataArtists.items.map((item: any) => [item.name, item.images[0].url]);
         setTopArtists(dataArtists);
 
-        const llm = await fetch(`http://localhost:8000/spotify/get-data/${"me|description"}`, {
+        const albumsResponse = await fetch(`http://localhost:8000/spotify/get-data/${"me|top|albums"}`, {
           credentials: 'include', // Include credentials (cookies) in the request
         });
+        const dataAlbums = await albumsResponse.json();
+        setTopAlbums(dataAlbums)
 
       } catch (error) {
         console.error('Error fetching user data:', error);
